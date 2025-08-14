89b563abe7b8b3ab56bf4557bcf2d18aa2bd160e
okotinek1313 <jhuotattia@gmail.com>
2025-08-13 22:27:49 -0400

    set up Clerk Auth
    Adding a DB tommorow

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import AppRouter from './AppRouter'
+import AppRouter from './AppRouter';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 export default function App() {
-  return(
+  return (
     <>
+      <div className="fixed top-4 right-4">
+        <SignedOut>
+          <SignInButton mode="modal" />
+        </SignedOut>
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+      </div>
       <AppRouter />
     </>
-  )
+  );
 }
