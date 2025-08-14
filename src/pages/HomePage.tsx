15719cf8a5739f275c8f7a8fed00b51a64f98537
okotinek1313 <jhuotattia@gmail.com>
2025-08-13 20:52:10 -0400

    restarting cuz i fucked up

--- src/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import * as LucideIcon from 'lucide-react'
-
-export default function HomePage() {
-    return(
-        <div id="HomePage" className="bg-grey-30 h-screen w-screen overscroll-y-none">
-            
-        </div>
-    )
-}
