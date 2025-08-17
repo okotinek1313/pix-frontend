84072743dd25b64b209ef5cdcfd88da9fcbc8fc8
okotinek1313 <jhuotattia@gmail.com>
2025-08-17 02:00:28 -0400

    Updated Stuff

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import Sidebar from '../components/Sidebar'
 import { useNavigate } from 'react-router-dom'
 
 // Const Decleration
-const LucideIconSize : number = 36
+const normalLucideIconSize : number = 36
 
 export default function HomePage() {
     const navigate = useNavigate()
@@ -15,13 +15,17 @@ export default function HomePage() {
         navigate('/settings')
     }
 
+    const navigateLocalContent = () => {
+        navigate('/local-content')
+    }
+
     return(
         <>
             <div className="w-screen h-screen !overflow-hidden">
                 <Sidebar>
                     <div className="w-full h-16" id={"sidebarHeader"}>
-                        <LucideIcon.House className={"cursor-pointer lucide-icon text-white left-[1.5%] top-[1.5%] absolute"} size={LucideIconSize} onClick={navigateHome}/>
-                        <LucideIcon.Settings className={"cursor-pointer lucide-icon text-white right-[1.5%] top-[1.5%] absolute"} size={LucideIconSize} onClick={navigateSettings} />
+                        <LucideIcon.House className={"cursor-pointer lucide-icon text-white left-[1.5%] top-[1.5%] absolute"} size={normalLucideIconSize} onClick={navigateHome}/>
+                        <LucideIcon.Settings className={"cursor-pointer lucide-icon text-white right-[1.5%] top-[1.5%] absolute"} size={normalLucideIconSize} onClick={navigateSettings} />
                     </div>
                 </Sidebar>
             </div>
