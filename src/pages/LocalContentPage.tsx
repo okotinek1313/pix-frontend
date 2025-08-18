20a0410bd9129de7ff8c57d2a005e38718c27708
okotinek1313 <jhuotattia@gmail.com>
2025-08-18 14:12:55 -0400

    Fixed Not Found Page

--- src/pages/LocalContentPage.tsx
+++ src/pages/LocalContentPage.tsx
@@ -1,8 +1,7 @@
 export default function LocalContentPage() {
     return(
         <div className="w-screen h-screen bg-grey-30 overflow-hidden">
-            <h1 className="mx-auto text-grey-40">404</h1>
-            <h3 className="mx-auto text-grey-40">Page Not Found :(</h3>
+            
         </div>
     )
 }
--- src/pages/NotfoundPage.tsx
+++ src/pages/NotfoundPage.tsx
@@ -1,7 +1,8 @@
 export default function NotFoundPage() {
     return(
         <div className="w-full h-full overflow-hidden">
-            
+            <h1 className="mx-auto text-grey-40">404</h1>
+            <h3 className="mx-auto text-grey-40 top[20%]">Page Not Found :(</h3>
         </div>
     )
 }
